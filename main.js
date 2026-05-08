d852ba40192eb321fb269d02d7b6a001919a9cc8
mohamedelmallhy-prog <mohamedelmallhy@gmail.com>
2026-05-08 09:56:43 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,136 @@
+//     window.alert("Hello From Js File");
+// console.log("Hllo from Js file")
+// console.log("log");
+// console.error("error");
+// console.table(["mohamed","adel","elmllahy"])
+
+
+//  Data Types Intro
+//     -string
+//     -number
+//     -Array=>Object
+//     -object
+//     -Boolean
+
+//  console.log("mohamed Adel");
+//  console.log(typeof "mohamed Adel");
+//  console.log(typeof 5000);
+//  console.log(typeof 5000.99);
+//  console.log(typeof [10,15,17]);
+//  console.log(typeof ["mohamed","Adel","mohamed"]);
+//  console.log(typeof {name:"mohamed", age:17, country:"Eg"});
+//  console.log(typeof true);  
+//  console.log(typeof false);
+//  console.log(typeof undefined);
+//  console.log(typeof null);
+
+// var a=1;
+// var a=2;
+// console.log(a);
+//  console.log("log") ;
+//  console.error("error") ;
+
+// document.write('<p>test</p>')
+// document.write('<p>test</p>')
+// document.write('<p>test</p>')
+// document.write('<p>test</p>')
+// document.write('<p>test</p>')
+// console.log("mohamed")
+
+// var a=20;
+// var b=10;
+// console.log(a+b);    
+// let name="mohamed";
+// let age=19;
+// console.log(`my name is ${name} and I am ${age} years old`)
+
+// let name=10;
+//     name -= 5;
+//     console.log(name);
+
+
+//     console.log(+'4 mohamed');
+//     console.log(Number('4 mohamed'));  
+//      console.log(parseInt('4 mohamed'));
+  
+
+
+
+    //  تحويل النصوص الي ارقام 
+//     console.log(+'4');
+//     console.log(Number('4'));
+//     console.log(Number.MAX_VALUE);
+//    console.log(parseInt('4.5'));
+//     console.log(parseFloat('4.5'));
+
+//      console.log(Number.MAX_SAFE_INTEGER);
+//     console.log(Number.MIN_SAFE_INTEGER);
+//     console.log(Number.isInteger(10));
+//     console.log(Number.isNaN('mohamed'-5));
+
+//     console.log(Math.abs(-10));
+//     console.log(Math.sqrt(2));
+//     console.log(Math.pow(2,3)); 
+//     console.log(Math.round(4.5));
+//     console.log(Math.floor(4.9));
+//     console.log(Math.min(1,2,5,15));
+//     console.log(Math.max(1,2,5,15));
+
+
+
+//     let mohamed =window.prompt()
+//     console.log(20 * mohamed +'جنيه' );   
+
+
+//     let x=100;
+//     console.log(typeof 100..  toString());
+
+//       let names = ["mohamed","adel","mohamed"];
+//     names[0]=mohamed;
+//       console.log(names);
+
+//       let name = ['mohamed','adel','mohamed'];
+//       name.push('mohamed');
+//       console.log(name);
+
+
+
+    //   let name = ["mohamed","adel","mohamed"];
+    //   name.splice(1,0,'mmmmm');
+    //   console.log(name);
+
+    //     let name = ["mohamed","adel","mohamed"];
+    //  console.log( name.slice(0,2));
+    //   console.log(name);
+
+
+      let arr = ['mohamed','adel','mohamed' ,' amed'];
+      console.log(arr.reverse());
+
+
+
+    //   let askemail = window.prompt("what is your email ?")
+    //   let email = 'mohamedelmallhy@gmail.com';
+    //   console.log(askemail.toLowerCase().trim()== email);
+
+// if
+// let name1 = prompt('name1');
+// let name2 = prompt('name2');
+// if (name1 > name2)
+// {
+//     console.log('Hello');
+// }
+
+let role = prompt('what is your role ?');
+if (role == 'admin')
+{
+    document.write('Update, Delete , Create');
+}
+else if (role == 'moderator')
+{
+    document.write('Update, Create');
+}
+else
+{
+    document.write('hello user');
+}
