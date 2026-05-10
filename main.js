4f7bc1d54b958b34543a50e0c5a1e988aa701206
mohamedelmallhy-prog <mohamedelmallhy@gmail.com>
2026-05-10 22:32:00 +0200

    remove comments

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-//     window.alert("Hello From Js File");
-// console.log("Hllo from Js file")
-// console.log("log");
-// console.error("error");
-// console.table(["mohamed","adel","elmllahy"])
+    window.alert("Hello From Js File");
+console.log("Hllo from Js file")
+console.log("log");
+console.error("error");
+console.table(["mohamed","adel","elmllahy"])
 
 
 //  Data Types Intro
@@ -12,106 +12,106 @@
 //     -object
 //     -Boolean
 
-//  console.log("mohamed Adel");
-//  console.log(typeof "mohamed Adel");
-//  console.log(typeof 5000);
-//  console.log(typeof 5000.99);
-//  console.log(typeof [10,15,17]);
-//  console.log(typeof ["mohamed","Adel","mohamed"]);
-//  console.log(typeof {name:"mohamed", age:17, country:"Eg"});
-//  console.log(typeof true);  
-//  console.log(typeof false);
-//  console.log(typeof undefined);
-//  console.log(typeof null);
+ console.log("mohamed Adel");
+ console.log(typeof "mohamed Adel");
+ console.log(typeof 5000);
+ console.log(typeof 5000.99);
+ console.log(typeof [10,15,17]);
+ console.log(typeof ["mohamed","Adel","mohamed"]);
+ console.log(typeof {name:"mohamed", age:17, country:"Eg"});
+ console.log(typeof true);  
+ console.log(typeof false);
+ console.log(typeof undefined);
+ console.log(typeof null);
 
-// var a=1;
-// var a=2;
-// console.log(a);
-//  console.log("log") ;
-//  console.error("error") ;
+var a=1;
+var a=2;
+console.log(a);
+ console.log("log") ;
+ console.error("error") ;
 
-// document.write('<p>test</p>')
-// document.write('<p>test</p>')
-// document.write('<p>test</p>')
-// document.write('<p>test</p>')
-// document.write('<p>test</p>')
-// console.log("mohamed")
+document.write('<p>test</p>')
+document.write('<p>test</p>')
+document.write('<p>test</p>')
+document.write('<p>test</p>')
+document.write('<p>test</p>')
+console.log("mohamed")
 
-// var a=20;
-// var b=10;
-// console.log(a+b);    
-// let name="mohamed";
-// let age=19;
-// console.log(`my name is ${name} and I am ${age} years old`)
+var a=20;
+var b=10;
+console.log(a+b);    
+let name="mohamed";
+let age=19;
+console.log(`my name is ${name} and I am ${age} years old`)
 
-// let name=10;
-//     name -= 5;
-//     console.log(name);
+let name=10;
+    name -= 5;
+    console.log(name);
 
 
-//     console.log(+'4 mohamed');
-//     console.log(Number('4 mohamed'));  
-//      console.log(parseInt('4 mohamed'));
+    console.log(+'4 mohamed');
+    console.log(Number('4 mohamed'));  
+     console.log(parseInt('4 mohamed'));
   
 
 
 
     //  تحويل النصوص الي ارقام 
-//     console.log(+'4');
-//     console.log(Number('4'));
-//     console.log(Number.MAX_VALUE);
-//    console.log(parseInt('4.5'));
-//     console.log(parseFloat('4.5'));
+    console.log(+'4');
+    console.log(Number('4'));
+    console.log(Number.MAX_VALUE);
+   console.log(parseInt('4.5'));
+    console.log(parseFloat('4.5'));
 
-//      console.log(Number.MAX_SAFE_INTEGER);
-//     console.log(Number.MIN_SAFE_INTEGER);
-//     console.log(Number.isInteger(10));
-//     console.log(Number.isNaN('mohamed'-5));
+     console.log(Number.MAX_SAFE_INTEGER);
+    console.log(Number.MIN_SAFE_INTEGER);
+    console.log(Number.isInteger(10));
+    console.log(Number.isNaN('mohamed'-5));
 
-//     console.log(Math.abs(-10));
-//     console.log(Math.sqrt(2));
-//     console.log(Math.pow(2,3)); 
-//     console.log(Math.round(4.5));
-//     console.log(Math.floor(4.9));
-//     console.log(Math.min(1,2,5,15));
-//     console.log(Math.max(1,2,5,15));
-
-
-
-//     let mohamed =window.prompt()
-//     console.log(20 * mohamed +'جنيه' );   
-
-
-//     let x=100;
-//     console.log(typeof 100..  toString());
-
-//       let names = ["mohamed","adel","mohamed"];
-//     names[0]=mohamed;
-//       console.log(names);
-
-//       let name = ['mohamed','adel','mohamed'];
-//       name.push('mohamed');
-//       console.log(name);
+    console.log(Math.abs(-10));
+    console.log(Math.sqrt(2));
+    console.log(Math.pow(2,3)); 
+    console.log(Math.round(4.5));
+    console.log(Math.floor(4.9));
+    console.log(Math.min(1,2,5,15));
+    console.log(Math.max(1,2,5,15));
 
 
 
-    //   let name = ["mohamed","adel","mohamed"];
-    //   name.splice(1,0,'mmmmm');
-    //   console.log(name);
-
-    //     let name = ["mohamed","adel","mohamed"];
-    //  console.log( name.slice(0,2));
-    //   console.log(name);
+    let mohamed =window.prompt()
+    console.log(20 * mohamed +'جنيه' );   
 
 
-    //   let arr = ['mohamed','adel','mohamed' ,' amed'];
-    //   console.log(arr.reverse());
+    let x=100;
+    console.log(typeof 100..  toString());
+
+      let names = ["mohamed","adel","mohamed"];
+    names[0]=mohamed;
+      console.log(names);
+
+      let name = ['mohamed','adel','mohamed'];
+      name.push('mohamed');
+      console.log(name);
 
 
 
-    //   let askemail = window.prompt("what is your email ?")
-    //   let email = 'mohamedelmallhy@gmail.com';
-    //   console.log(askemail.toLowerCase().trim()== email);
+      let name = ["mohamed","adel","mohamed"];
+      name.splice(1,0,'mmmmm');
+      console.log(name);
+
+        let name = ["mohamed","adel","mohamed"];
+     console.log( name.slice(0,2));
+      console.log(name);
+
+
+      let arr = ['mohamed','adel','mohamed' ,' amed'];
+      console.log(arr.reverse());
+
+
+
+      let askemail = window.prompt("what is your email ?")
+      let email = 'mohamedelmallhy@gmail.com';
+      console.log(askemail.toLowerCase().trim()== email);
 
 // if
 // let name1 = prompt('name1');
@@ -121,188 +121,191 @@
 //     console.log('Hello');
 // }
 
-// let role = prompt('what is your role ?');
-// if (role == 'admin')
-// {
-//     document.write('Update, Delete , Create');
-// }
-// else if (role == 'moderator')
-// {
-//     document.write('Update, Create');
-// }
-// else
-// {
-//     document.write('hello user');
-// }
+let role = prompt('what is your role ?');
+if (role == 'admin')
+{
+    document.write('Update, Delete , Create');
+}
+else if (role == 'moderator')
+{
+    document.write('Update, Create');
+}
+else
+{
+    document.write('hello user');
+}
 
 
 // طريقه اخرى لكتابه if
 
-// let age = prompt('what is your age ?');
-// let result = age >= 18 ? 'hello user' 
-// :age == 18? 'you are 18 now' : 'No you are very young';
-// console.log(result);
+let age = prompt('what is your age ?');
+let result = age >= 18 ? 'hello user' 
+:age == 18? 'you are 18 now' : 'No you are very young';
+console.log(result);
 
 
-//Conditional statements switch
+//  Conditional statements switch
 
-// let role = prompt('what is your role ?');
-// switch (role)
-// {
-//     case 'admin':
-//         document.write('Update, Delete , Create');
-//         break;
-//     case 'moderator':
-//         document.write('Update, Create');
-//         break;
-//      case 'editor':
-//         document.write('Update');
-//         break;   
-//     default:
-//         document.write('hello user');
-// }
+let  role = prompt('what is your role ?');
+switch (role)
+{
+    case 'admin':
+        document.write('Update, Delete , Create');
+        break;
+    case 'moderator':
+        document.write('Update, Create');
+        break;
+     case 'editor':
+        document.write('Update');
+        break;   
+    default:
+        document.write('hello user');
+}
 
-
-
-//  الفرق بين if و switch
-
-//  if بنستخدمها لو عندنا شرط او مقارنه
-
-// let age = 20;
-
-// if(age >= 18){
-//     console.log("Adult");
-// }
-
-
-//  switch بنستخدمها لو عندنا حالات كتير لنفس القيمه
-
-// let day = 2;
-
-// switch(day){
-
-//     case 1:
-//         console.log("Saturday");
-//         break;
-
-//     case 2:
-//         console.log("Sunday");
-//         break;
-
-//     default:
-//         console.log("Unknown");
-// }
 
 
 //  الفرق بين if و switch
 
 //  if بنستخدمها لو عندنا شرط او مقارنه
 
-// let age = 20;
+let age = 20;
 
-// if(age >= 18){
-//     console.log("Adult");
-// }
+if(age >= 18){
+    console.log("Adult");
+}
 
 
 //  switch بنستخدمها لو عندنا حالات كتير لنفس القيمه
 
-// let day = 2;
+let day = 2;
 
-// switch(day){
+switch(day){
 
-//     case 1:
-//         console.log("Saturday");
-//         break;
+    case 1:
+        console.log("Saturday");
+        break;
 
-//     case 2:
-//         console.log("Sunday");
-//         break;
+    case 2:
+        console.log("Sunday");
+        break;
 
-//     default:
-//         console.log("Unknown");
-// }
+    default:
+        console.log("Unknown");
+}
+
+
+//  الفرق بين if و switch
+
+//  if بنستخدمها لو عندنا شرط او مقارنه
+
+let age = 20;
+
+if(age >= 18){
+    console.log("Adult");
+}
+
+
+//  switch بنستخدمها لو عندنا حالات كتير لنفس القيمه
+
+let day = 2;
+
+switch(day){
+
+    case 1:
+        console.log("Saturday");
+        break;
+
+    case 2:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Unknown");
+}
 
 
 // loop (1) for loop
 
-//mohamed 1
-//mohamed 2
-//mohamed 3
+// mohamed 1
+// mohamed 2
+// mohamed 3
 
-    // for(let i=1; i<=3; i++)
-    // {
-    //     console.log(`mohamed` +i);
-    // }    
+    for(let i=1; i<=3; i++)
+    {
+        console.log(`mohamed` +i);
+    }    
 
 // loop (2)
 
-    // let names = ['mohamed','adel','mohamed'];
-    // for(let i=0; i<names.length; i++)
-    // {
-    //     console.log(names[i]);
-    // }
+    let names = ['mohamed','adel','mohamed'];
+    for(let i=0; i<names.length; i++)
+    {
+        console.log(names[i]);
+    }
 
-    // let x = 'mohamed';
-    // for(let i=0; i<x.length; i++)
-    // {
-    //     console.log(x[i]);
-    // }
+    let x = 'mohamed';
+    for(let i=0; i<x.length; i++)
+    {
+        console.log(x[i]);
+    }
 
     // nasted loop(3)
-//   let cars = ['BMW', 'Audi', 'Toyota'];
+  let cars = ['BMW', 'Audi', 'Toyota'];
 
-// let models = ['2020', '2021', '2022'];
+let models = ['2020', '2021', '2022'];
 
-// for(let i = 0; i < cars.length; i++)
-// {
-//     console.log(`cars: ${cars[i]}`);
+for(let i = 0; i < cars.length; i++)
+{
+    console.log(`cars: ${cars[i]}`);
 
-//     for(let j = 0; j < models.length; j++)
-//     {
-//         console.log(`models: ${models[j]}`);
-//     }
-//      console.log('-------------------');
-// }
+    for(let j = 0; j < models.length; j++)
+    {
+        console.log(`models: ${models[j]}`);
+    }
+     console.log('-------------------');
+}
 
 // break and continue (4)
 
-// let users = ['mohamed', 'adel', 1,2,3, 'mohamed', 'amed'];
+let users = ['mohamed', 'adel', 1,2,3, 'mohamed', 'amed'];
 
-// for(let i = 0; i < users.length; i++)
-// {
-//     if(typeof users[i] == 'number')
-//     {
-//         continue;
-//     }
+for(let i = 0; i < users.length; i++)
+{
+    if(typeof users[i] == 'number')
+    {
+        continue;
+    }
 
-//     console.log(users[i]);
-// }
+    console.log(users[i]);
+}
 
-// let users = ['mohamed', 'adel', 'mohamed', 'amed'];
+let users = ['mohamed', 'adel', 'mohamed', 'amed'];
 
-// for(let i = 0; i < users.length; i++)
-// {
-//     if(users[i] == 'adel')
-//     {
-//         break;
-//     }
+for(let i = 0; i < users.length; i++)
+{
+    if(users[i] == 'adel')
+    {
+        break;
+    }
 
-//     console.log(users[i]);
-// }
+    console.log(users[i]);
+}
 
 
 // while loop-do while loop (5)
 
-// let i=0;
-// while(i<3)
-// {
-//     document.write('<h1>Hello</h1>')
-//     i++;
-// }
+let i=0;
+while(i<3)
+{
+    document.write('<h1>Hello</h1>')
+    i++;
+}
 
-// let i=0;
-// do{
-//     document.write('hello');
-//     i++;
-// }while(i<3);
+let i=0;
+do{
+    document.write('hello');
+    i++;
+}while(i<3);
+
+
+
