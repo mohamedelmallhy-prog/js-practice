74271aade0d595efe9d8bb4827b0dcb67b5f465a
mohamedelmallhy-prog <mohamedelmallhy@gmail.com>
2026-05-20 22:20:02 +0300

    what is object in javascript

---
+++ what is object in javascript.js
@@ -0,0 +1,15 @@
+let car = {
+    title : 'BMW',
+    price : 100000,
+    color : 'red',
+    models : 2020,
+
+    hello: function(){
+
+        return 'hello';
+        }
+       
+     };
+
+     console.log(car); 
+
